294115fef848c31542273400e69659b0ca33216a
YusukeAkagi <yusuke.akagi@gmo.media>
2025-03-01 14:30:44 +0000

    Add a full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    #ここで関数を定義しており、引数がpage_titleの中に格納される。
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
 end
