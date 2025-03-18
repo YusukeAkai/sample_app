e41ff3a9357fb90973961e138ceaa8aba75611fc
YusukeAkagi <yusuke.akagi@gmo.media>
2025-03-18 09:55:32 +0000

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,17 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user &.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user&.authenticate(params[:session][:password])
       reset_session
       # ログインする際に、セッション固定と呼ばれる攻撃を防ぐためのコード
-      log_in user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      # module SessionsHelperのrememberヘルパーメソッドを呼び出す
+      log_in @user
       # module SessionsHelperのlog_inメソッドを呼び出す
       # メソッド内では、userのidがsession[:user_id]に代入される
       # ブラウザ内の一時cookiesに暗号化されたユーザーIDが自動的に作成される。
-      redirect_to user
+      redirect_to @user
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       # redirect_to user_url(user) と同等のコード
     else
@@ -20,10 +22,11 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     # ヘルパーメソッドのlog_outを呼び出す
     # セッションがリセットされる。
     redirect_to root_url, status: :see_other
     # httpのステータスは303 See Other
   end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,62 @@
 class User < ApplicationRecord
-  before_save { self.email.downcase! }
+  attr_accessor :remember_token
+
+  before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 }, 
-  format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }, uniqueness: true
   has_secure_password
   # ハッシュかしたパスワードをデータベースのpassword_digest属性に保存できる。
   # 電子署名でやった。
   # authenticateメソッドが使えるようになる。
-  validates :password, presence: true, length: {minimum: 8}
+  validates :password, presence: true, length: { minimum: 8 }
 
-  def User.digest(string)
-    # クラスメソッドの定義
-    # テスト用の仮のアカウント登録をする。
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                              BCrypt::Engine.cost
-    # テスト中はコストパラメータを最小にし、本番環境では通常の高いコストで計算する。
-    BCrypt::Password.create(string, cost: cost)
-    # ここでパスワードが生成されている。
-    # costが大きいほどパスワードのハッシュが複雑になり、解読に時間がかかる。
+  class << self
+    def digest(string)
+      # クラスメソッドの定義
+      # ここでのselfはUserクラス自体を指す。
+      # テスト用の仮のアカウント登録をする。
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      # テスト中はコストパラメータを最小にし、本番環境では通常の高いコストで計算する。
+      BCrypt::Password.create(string, cost: cost)
+      # ここでパスワードのハッシュが生成されている。
+      # costが大きいほどパスワードのハッシュが複雑になり、解読に時間がかかる。
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+      # ランダムなトークンを生成する。
+      # このトークンは実行するたびに違うものが出力される。
+    end
   end
+
+  def remember
+    self.remember_token = User.new_token
+    # selfを書くことによって、remember_tokenという名前の属性を作成している。
+    update_attribute(:remember_digest, User.digest(remember_token))
+    # このattributeが属性に対応している。
+    # remember_digestっていう表の属性に対して、digestメソッドを使用することでremember_tokenをハッシュ化して代入している。
+    remember_digest
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    # ここでのremember_tokenは一番上で定義されたattr_accessor :remember_tokenとは異なる
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # 記憶ダイジェストをクリアにする。
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  def session_token
+    remember_digest || remember
+  end
+  
 end
