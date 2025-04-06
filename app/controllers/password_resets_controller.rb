27a4e112c2b5ed3f61950766e4716313e343baba
yukke04500 <akagiyusuke@icloud.com>
2025-04-06 16:31:51 +0900

    Add password reset

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,66 @@
+class PasswordResetsController < ApplicationController
+  before_action :get_user, only: %i[edit update]
+  before_action :valid_user, only: %i[edit update]
+  before_action :check_expiration, only: %i[edit update]
+
+  def new; end
+
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    # @userがUserモデルのインスタンス変数
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      # 　この2つのメソッドは，model内で定義する．
+      flash[:info] = 'Email sent with password reset instructions'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'Email address not found'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render 'edit', status: :unprocessable_entity
+    elsif @user.update(user_params)
+      reset_session
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      # パスワードの再設定が完了 => reset_digestをnilにする
+      flash[:success] = 'Password has been reset.'
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+end
+
+  private
+
+def user_params
+  params.require(:user).permit(:password, :password_confirmation)
+  # <ActionController::Parameters {"password"=>"akagiyusuke", "password_confirmation"=>"akagiyusuke"} permitted: true>
+  # 　こんな感じの戻り値が定義される．
+end
+
+def get_user
+  @user = User.find_by(email: params[:email])
+end
+
+def valid_user
+  unless @user && @user.activated? &&
+         @user.authenticated?(:reset, params[:id])
+    redirect_to root_url
+  end
+end
+
+def check_expiration
+  return unless @user.password_reset_expired?
+
+  flash[:danger] = 'Password reset has expired.'
+  redirect_to new_password_reset_url, status: :see_other
+end
